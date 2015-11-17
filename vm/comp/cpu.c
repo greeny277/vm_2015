3793a0f3c54a47060234634482c05d430ad98b72
Christian Bay <christian.bay@studium.fau.de>
2015-11-17 14:14:12 +0100

    CPU: Implement all CMP opcodes

--- vm/comp/cpu.c
+++ vm/comp/cpu.c
@@ -697,10 +697,10 @@ cpu_step(void *_cpu_state) {
 
 				subtrahend = cpu_read_byte_from_reg(s_op.op1_reg, s_op.is_op1_high);
 				if(s_op.op2_reg != 0)
-					minuend = cpu_read_word_from_reg(s_op.op2_reg);
+					minuend = cpu_read_byte_from_reg(s_op.op2_reg, s_op.is_op2_high);
 				else
-					minuend = cpu_peek_word_from_ram(cpu_state, s_op.op2_mem);
-				uint8_t result = minuend-subtrahend;
+					minuend = cpu_peek_byte_from_ram(cpu_state, s_op.op2_mem);
+				uint8_t result = minuend - subtrahend;
 
 				set_eflag_arith(cpu_state, minuend, subtrahend, result,
 						EIGHT_BIT,SUBTRACTION);
@@ -708,6 +708,62 @@ cpu_step(void *_cpu_state) {
 			}
 			break;
 		}
+		case 0x39: {
+			/*Compare r32 with r/m32. */
+			if(!cpu_decode_RM(cpu_state, &s_op, !EIGHT_BIT, !IMMEDIATE)){
+				uint32_t minuend, subtrahend;
+
+				subtrahend = cpu_read_word_from_reg(s_op.op1_reg);
+				if(s_op.op2_reg != 0)
+					minuend = cpu_read_word_from_reg(s_op.op2_reg);
+				else
+					minuend = cpu_peek_word_from_ram(cpu_state, s_op.op2_mem);
+				uint32_t result = minuend - subtrahend;
+
+				set_eflag_arith(cpu_state, minuend, subtrahend, result,
+						!EIGHT_BIT,SUBTRACTION);
+				return true;
+			}
+			break;
+		}
+		case 0x3A: {
+			/* Compare r/m8 with r8. */
+			if(!cpu_decode_RM(cpu_state, &s_op, EIGHT_BIT, !IMMEDIATE)){
+				uint8_t minuend, subtrahend;
+
+				minuend = cpu_read_byte_from_reg(s_op.op1_reg, s_op.is_op1_high);
+				if(s_op.op2_reg != 0)
+					subtrahend = cpu_read_byte_from_reg(s_op.op2_reg, s_op.is_op2_high);
+				else
+					subtrahend = cpu_peek_byte_from_ram(cpu_state, s_op.op2_mem);
+				uint8_t result = minuend - subtrahend;
+
+				set_eflag_arith(cpu_state, minuend, subtrahend, result,
+						EIGHT_BIT,SUBTRACTION);
+				return true;
+			}
+			break;
+		}
+
+		case 0x3B: {
+			/* Compare r/m32 with r32. */
+			if(!cpu_decode_RM(cpu_state, &s_op, !EIGHT_BIT, !IMMEDIATE)){
+				uint32_t minuend, subtrahend;
+
+				minuend = cpu_read_word_from_reg(s_op.op1_reg);
+				if(s_op.op2_reg != 0)
+					subtrahend = cpu_read_word_from_reg(s_op.op2_reg);
+				else
+					subtrahend = cpu_peek_word_from_ram(cpu_state, s_op.op2_mem);
+				uint32_t result = minuend - subtrahend;
+
+				set_eflag_arith(cpu_state, minuend, subtrahend, result,
+						!EIGHT_BIT,SUBTRACTION);
+				return true;
+			}
+			break;
+		}
+
 		default:
 			break;
 	}
