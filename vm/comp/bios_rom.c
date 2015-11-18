8bb23316979766dbec7f2bc3c4041cc6520a4208
Christian Bay <christian.bay@studium.fau.de>
2015-11-18 20:33:10 +0100

    BIOS: Rewrite bios a bit

--- vm/comp/bios_rom.c
+++ vm/comp/bios_rom.c
@@ -8,34 +8,27 @@
 
 #include "bios_rom.h"
 
-#define ROM_MEM_BASE 0xE000
-#define ROM_SIZE     4*1024
-
-struct cpssp {
-	/** ports */
-	struct sig_host_bus *port_host;
-
-	/** state */
-	char rom[ROM_SIZE];
-};
+/* Declaration of static methods */
+static bool rom_readb(void *_rom_state, uint32_t addr, uint8_t *valp);
+static bool rom_writeb(void *_rom_state, uint32_t addr, uint8_t val);
 
 static bool
-rom_readb(void *_cpssp, uint32_t addr, uint8_t *valp){
+rom_readb(void *_rom_state, uint32_t addr, uint8_t *valp){
 
-	struct cpssp* cpssp = (struct cpssp*) _cpssp;
+	rom_state *rom_state = (struct rom_state *) _rom_state;
 	uint32_t offset = addr - ROM_MEM_BASE;
 
 	if(offset >= ROM_SIZE){
 		return false;
 	}
 
-	*valp = cpssp->rom[offset];
+	*valp = rom_state->rom[offset];
 
 	return true;
 }
 
 static bool
-rom_writeb(void *_cpssp, uint32_t addr, uint8_t val){
+rom_writeb(void *_rom_state, uint32_t addr, uint8_t val){
 	/* We dont write into ROM */
 
 	/* Ignoring */
@@ -45,23 +38,23 @@ rom_writeb(void *_cpssp, uint32_t addr, uint8_t val){
 void *
 bios_rom_create(struct sig_host_bus *port_host, const char *rf)
 {
-	struct cpssp *cpssp;
+	rom_state *rom_state;
 	static const struct sig_host_bus_funcs hf = {
 		.readb = rom_readb,
 		.writeb = rom_writeb
 	};
 
-	cpssp = malloc(sizeof(struct cpssp));
-	assert(cpssp != NULL);
+	rom_state = malloc(sizeof(struct rom_state));
+	assert(rom_state != NULL);
 
-	cpssp->port_host = port_host;
+	rom_state->port_host = port_host;
 
 	/* Get size of BIOS file */
 	struct stat s;
 	assert(0 == stat(rf, &s));
 	off_t bios_size = s.st_size;
 
-	/* Check if BIOS file fits into ROM */
+	/* Check if BIOS file fits into ROM component */
 	assert(bios_size <= ROM_SIZE);
 
 	/* Open bios file */
@@ -69,22 +62,22 @@ bios_rom_create(struct sig_host_bus *port_host, const char *rf)
 	assert(f != NULL);
 
 	/* Zero out rom buffer */
-	memset(cpssp->rom, 0, ROM_SIZE);
+	memset(rom_state->rom, 0, ROM_SIZE);
 
 	/* Read BIOS file in rom buffer */
-	assert(bios_size == fread(cpssp->rom, sizeof(char), bios_size, f));
+	assert(bios_size == fread(rom_state->rom, 1, bios_size, f));
 
 	/* Close filepointer */
 	fclose(f);
 
-	sig_host_bus_connect(port_host, cpssp, &hf);
+	sig_host_bus_connect(port_host, rom_state, &hf);
 
-	return cpssp;
+	return rom_state;
 }
 
 void
-bios_rom_destroy(void *_cpssp)
+bios_rom_destroy(void *_rom_state)
 {
-	free(_cpssp);
+	free(_rom_state);
 }
 /* vim: set tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab : */
