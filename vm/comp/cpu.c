c3c62ba9604688c1446d06e189b9cf6c9104494b
Cuong Bui <cuong.bui@fau.de>
2015-11-18 11:47:51 +0100

    CPU: 0xFE implemented

--- vm/comp/cpu.c
+++ vm/comp/cpu.c
@@ -750,6 +750,24 @@ cpu_step(void *_cpu_state) {
 
 		#include "cpu_JumpInst.c"
 
+		case 0xFE: {
+			/* Increment r/m byte by 1 */
+			if(!cpu_decode_RM(cpu_state, &s_op, EIGHT_BIT)){
+				uint8_t src;
+				if(s_op.regmem_type == MEMORY){
+					src = cpu_peek_byte_from_mem(cpu_state, s_op.regmem_mem);
+					src++;
+					cpu_write_byte_in_mem(cpu_state, src, s_op.regmem_mem);
+				} else {
+					src = cpu_read_byte_from_reg(s_op.reg, s_op.regmem_type == REGISTER_HIGH);
+					src++;
+					cpu_write_byte_in_reg(src, s_op.regmem_reg, s_op.regmem_type == REGISTER_HIGH);
+				}
+			return true;
+			}
+			break;
+		}
+		
 
 		case 0x0f: {
 			/* The opcode is two bytes long */
