cf6499d15eb0d57120bbeb7bd3cbf590b9c551bf
Cuong Bui <cuong.bui@fau.de>
2015-11-18 11:50:59 +0100

    CPU: 0xFF implemented

--- vm/comp/cpu.c
+++ vm/comp/cpu.c
@@ -767,7 +767,23 @@ cpu_step(void *_cpu_state) {
 			}
 			break;
 		}
-		
+
+		case 0xFF: {
+			if(!cpu_decode_RM(cpu_state, &s_op, !EIGHT_BIT)){
+				uint32_t src;
+				if(s_op.regmem_type == MEMORY){
+					src = cpu_peek_word_from_mem(cpu_state, s_op.regmem_mem);
+					src++;
+					cpu_write_word_in_mem(cpu_state, src, s_op.regmem_mem);
+				} else {
+					src = cpu_read_word_from_reg(s_op.reg);
+					src++;
+					cpu_write_word_in_reg(src, s_op.regmem_reg);
+				}
+			return true;
+			}
+			break;
+		}
 
 		case 0x0f: {
 			/* The opcode is two bytes long */
