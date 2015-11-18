57ff0129d9fda53bcef6cf7233affb0386cdc361
Christian Bay <christian.bay@studium.fau.de>
2015-11-18 12:19:38 +0100

    CPU: Implement RET instructions

--- vm/comp/cpu_stackInst.c
+++ vm/comp/cpu_stackInst.c
@@ -12,9 +12,62 @@ case 0x9A: {
 	/* CALL ptr32
 	 * Call far, absolute, address given in operand.
 	 */
-	int32_t abs = cpu_read_word_from_mem(cpu_state);
+	uint32_t abs = cpu_read_word_from_mem(cpu_state);
 	cpu_stack_push_doubleword(cpu_state, cpu_state->eip);
 	cpu_write_word_in_reg(abs, &(cpu_state->eip));
 
 	return true;
+}
+
+case 0xC3: {
+	/* Near return to calling procedure. */
+	cpu_write_word_in_reg(cpu_stack_pop_doubleword(cpu_state) , &(cpu_state->eip));
+
+	return true;
+}
+
+case 0xCB:{
+	/* Far return to calling procedure.
+	 * A return to a calling procedure located
+	 * in a different segment
+	 */
+	
+	/* No segments implemented yet */
+	fprintf(stderr, "No segments implemented yet\n");
+
+	break;
+}
+
+case 0xC2: {
+	/* RET imm16 */
+	
+	/* Near return to calling procedure and pop
+	 * imm16 bytes from stack.
+	 */
+	cpu_write_word_in_reg(cpu_stack_pop_doubleword(cpu_state) , &(cpu_state->eip));
+
+	uint16_t imm16;
+
+	uint8_t byte0, byte1;
+
+	byte0 = cpu_read_byte_from_mem(cpu_state);
+	byte1 = cpu_read_byte_from_mem(cpu_state);
+
+	imm16 = byte0;
+	imm16 |= (byte1 << 8);
+
+	cpu_write_word_in_reg(cpu_state->esp+imm16 , &(cpu_state->esp));
+
+	return true;
+}
+
+case 0xCA: {
+	/*  Far return to calling procedure and pop imm16
+	 *  bytes from stack.
+	 */
+
+	/* No segments implemented yet */
+	fprintf(stderr, "No segments implemented yet\n");
+
+	break;
 }
