898bc8088ab60f44e59c5858a7663ec101599d5d
Cuong Bui <cuong.bui@fau.de>
2015-11-21 17:24:48 +0100

    CPU: now correct version

--- vm/comp/instructionBlocks/cpu_incInst.c
+++ vm/comp/instructionBlocks/cpu_incInst.c
@@ -1,21 +1,3 @@
-		case 0xFE: {
-			/* Increment r/m byte by 1 */
-			if(!cpu_decode_RM(cpu_state, &s_op, EIGHT_BIT)){
-				uint8_t src;
-				if(s_op.regmem_type == MEMORY){
-					src = cpu_read_byte_from_mem(cpu_state, s_op.regmem_mem);
-					src++;
-					cpu_write_byte_in_mem(cpu_state, src, s_op.regmem_mem);
-				} else {
-					src = cpu_read_byte_from_reg(s_op.reg, s_op.regmem_type == REGISTER_HIGH);
-					src++;
-					cpu_write_byte_in_reg(s_op.regmem_reg, src ,IS_HIGH(s_op.regmem));
-				}
-			return true;
-			}
-			break;
-		}
-
 		case 0x40: {
 			/* Increment doubleword register eax by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->eax));
