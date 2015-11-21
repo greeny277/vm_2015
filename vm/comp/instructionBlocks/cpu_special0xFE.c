34165c5ebc1d30c633714b89b0278f0208313806
Cuong Bui <cuong.bui@fau.de>
2015-11-21 18:18:24 +0100

    CPU: 0xFE/1 wrong arithm

--- vm/comp/instructionBlocks/cpu_special0xFE.c
+++ vm/comp/instructionBlocks/cpu_special0xFE.c
@@ -17,16 +17,16 @@ case 0: {
 }
 
 case 1: {
-	/* decrement r/m byte by 1 */
+	/* Decrement r/m byte by 1 */
 	if(!cpu_decode_RM(cpu_state, &s_op, EIGHT_BIT)){
 		uint8_t src;
 		if(s_op.regmem_type == MEMORY){
 			src = cpu_read_byte_from_mem(cpu_state, s_op.regmem_mem);
-			src++;
+			src--;
 			cpu_write_byte_in_mem(cpu_state, src, s_op.regmem_mem);
 		} else {
 			src = cpu_read_byte_from_reg(s_op.reg, s_op.regmem_type == REGISTER_HIGH);
-			src++;
+			src--;
 			cpu_write_byte_in_reg(s_op.regmem_reg, src ,IS_HIGH(s_op.regmem));
 		}
 		return true;
