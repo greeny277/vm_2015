efaddc9d81bb848f034609587e9ef24fe5d97eeb
Cuong Bui <cuong.bui@fau.de>
2015-11-21 17:11:55 +0100

    CPU: 0xFF to special instructions

--- vm/comp/instructionBlocks/cpu_special0xFF.c
+++ vm/comp/instructionBlocks/cpu_special0xFF.c
@@ -13,5 +13,23 @@ case 4: {
 	}
 	break;
 }
+case 5: {
+	/* Increment r/m word by 1 */
+	if(!cpu_decode_RM(cpu_state, &s_op, !EIGHT_BIT)){
+		uint32_t src;
+		if(s_op.regmem_type == MEMORY){
+			src = cpu_read_word_from_mem(cpu_state, s_op.regmem_mem);
+			src++;
+			cpu_write_word_in_mem(cpu_state, src, s_op.regmem_mem);
+		} else {
+			src = cpu_read_word_from_reg(s_op.reg);
+			src++;
+			cpu_write_word_in_reg(s_op.regmem_reg, src);
+		}
+		return true;
+	}
+	break;
+}
+
 default:
 	break;
