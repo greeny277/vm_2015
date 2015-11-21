8aa71f39178e1347ea39d7edc3eebbae71f8d9dd
Cuong Bui <cuong.bui@fau.de>
2015-11-21 17:09:10 +0100

    CPU: cpu_Inst adapted to new inteface

--- vm/comp/instructionBlocks/cpu_incInst.c
+++ vm/comp/instructionBlocks/cpu_incInst.c
@@ -3,13 +3,13 @@
 			if(!cpu_decode_RM(cpu_state, &s_op, EIGHT_BIT)){
 				uint8_t src;
 				if(s_op.regmem_type == MEMORY){
-					src = cpu_peek_byte_from_mem(cpu_state, s_op.regmem_mem);
+					src = cpu_read_byte_from_mem(cpu_state, s_op.regmem_mem);
 					src++;
 					cpu_write_byte_in_mem(cpu_state, src, s_op.regmem_mem);
 				} else {
 					src = cpu_read_byte_from_reg(s_op.reg, s_op.regmem_type == REGISTER_HIGH);
 					src++;
-					cpu_write_byte_in_reg(src, s_op.regmem_reg, s_op.regmem_type == REGISTER_HIGH);
+					cpu_write_byte_in_reg(s_op.regmem_reg, src ,IS_HIGH(s_op.regmem));
 				}
 			return true;
 			}
@@ -21,13 +21,13 @@
 			if(!cpu_decode_RM(cpu_state, &s_op, !EIGHT_BIT)){
 				uint32_t src;
 				if(s_op.regmem_type == MEMORY){
-					src = cpu_peek_word_from_mem(cpu_state, s_op.regmem_mem);
+					src = cpu_read_word_from_mem(cpu_state, s_op.regmem_mem);
 					src++;
 					cpu_write_word_in_mem(cpu_state, src, s_op.regmem_mem);
 				} else {
 					src = cpu_read_word_from_reg(s_op.reg);
 					src++;
-					cpu_write_word_in_reg(src, s_op.regmem_reg);
+					cpu_write_word_in_reg(s_op.regmem_reg, src);
 				}
 			return true;
 			}
@@ -38,7 +38,7 @@
 			/* Increment doubleword register eax by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->eax));
 			src++;
-			cpu_write_word_in_reg(src, &(cpu_state->eax));
+			cpu_write_word_in_reg(&(cpu_state->eax), src);
 			return true;
 		}
 		
@@ -46,7 +46,7 @@
 			/* Increment doubleword register ecx by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ecx));
 			src++;
-			cpu_write_word_in_reg(src, &(cpu_state->ecx));
+			cpu_write_word_in_reg(&(cpu_state->ecx), src);
 			return true;
 		}
 
@@ -54,7 +54,7 @@
 			/* Increment doubleword register edx by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->edx));
 			src++;
-			cpu_write_word_in_reg(src, &(cpu_state->edx));
+			cpu_write_word_in_reg(&(cpu_state->edx), src);
 			return true;
 		}
 
@@ -62,7 +62,7 @@
 			/* Increment doubleword register ebx by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ebx));
 			src++;
-			cpu_write_word_in_reg(src, &(cpu_state->ebx));
+			cpu_write_word_in_reg(&(cpu_state->ebx), src);
 			return true;
 		}
 
@@ -70,7 +70,7 @@
 			/* Increment doubleword register esp by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->esp));
 			src++;
-			cpu_write_word_in_reg(src, &(cpu_state->esp));
+			cpu_write_word_in_reg(&(cpu_state->esp), src);
 			return true;
 		}
 
@@ -78,7 +78,7 @@
 			/* Increment doubleword register ebp by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ebp));
 			src++;
-			cpu_write_word_in_reg(src, &(cpu_state->ebp));
+			cpu_write_word_in_reg(&(cpu_state->ebp), src);
 			return true;
 		}
 
@@ -86,7 +86,7 @@
 			/* Increment doubleword register esi by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->esi));
 			src++;
-			cpu_write_word_in_reg(src, &(cpu_state->esi));
+			cpu_write_word_in_reg(&(cpu_state->esi), src);
 			return true;
 		}
 
@@ -94,6 +94,6 @@
 			/* Increment doubleword register edi by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->edi));
 			src++;
-			cpu_write_word_in_reg(src, &(cpu_state->edi));
+			cpu_write_word_in_reg(&(cpu_state->edi), src);
 			return true;
 		}
