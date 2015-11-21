4734dbf5271e1df7de6444955d74311cfcd9fa52
Cuong Bui <cuong.bui@fau.de>
2015-11-21 17:58:09 +0100

    CPU: 0xFE for dec instruction

---
+++ vm/comp/instructionBlocks/cpu_special0xFE.c
@@ -0,0 +1,39 @@
+case 0: {
+	/* Increment r/m byte by 1 */
+	if(!cpu_decode_RM(cpu_state, &s_op, EIGHT_BIT)){
+		uint8_t src;
+		if(s_op.regmem_type == MEMORY){
+			src = cpu_read_byte_from_mem(cpu_state, s_op.regmem_mem);
+			src++;
+			cpu_write_byte_in_mem(cpu_state, src, s_op.regmem_mem);
+		} else {
+			src = cpu_read_byte_from_reg(s_op.reg, s_op.regmem_type == REGISTER_HIGH);
+			src++;
+			cpu_write_byte_in_reg(s_op.regmem_reg, src ,IS_HIGH(s_op.regmem));
+		}
+		return true;
+	}
+	break;
+}
+
+case 1: {
+	/* decrement r/m byte by 1 */
+	if(!cpu_decode_RM(cpu_state, &s_op, EIGHT_BIT)){
+		uint8_t src;
+		if(s_op.regmem_type == MEMORY){
+			src = cpu_read_byte_from_mem(cpu_state, s_op.regmem_mem);
+			src++;
+			cpu_write_byte_in_mem(cpu_state, src, s_op.regmem_mem);
+		} else {
+			src = cpu_read_byte_from_reg(s_op.reg, s_op.regmem_type == REGISTER_HIGH);
+			src++;
+			cpu_write_byte_in_reg(s_op.regmem_reg, src ,IS_HIGH(s_op.regmem));
+		}
+		return true;
+	}
+	break;
+}
+
+
+dafault:
+	break;
