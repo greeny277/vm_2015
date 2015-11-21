577c9cf8ab8c3cf86b78b3864bfd2ce4ec8643d3
Cuong Bui <cuong.bui@fau.de>
2015-11-21 17:17:09 +0100

    CPU: 0xFE to special instructions

--- vm/comp/instructionBlocks/cpu_incInst.c
+++ vm/comp/instructionBlocks/cpu_incInst.c
@@ -16,24 +16,6 @@
 			break;
 		}
 
-		case 0xFF: {
-			/* Increment r/m word by 1 */
-			if(!cpu_decode_RM(cpu_state, &s_op, !EIGHT_BIT)){
-				uint32_t src;
-				if(s_op.regmem_type == MEMORY){
-					src = cpu_read_word_from_mem(cpu_state, s_op.regmem_mem);
-					src++;
-					cpu_write_word_in_mem(cpu_state, src, s_op.regmem_mem);
-				} else {
-					src = cpu_read_word_from_reg(s_op.reg);
-					src++;
-					cpu_write_word_in_reg(s_op.regmem_reg, src);
-				}
-			return true;
-			}
-			break;
-		}
-
 		case 0x40: {
 			/* Increment doubleword register eax by 1 */
 			uint32_t src = cpu_read_word_from_reg(&(cpu_state->eax));
