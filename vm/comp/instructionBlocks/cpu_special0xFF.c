d7cb517e4c48274dd72786edd8915cafbe5a2a86
Cuong Bui <cuong.bui@fau.de>
2015-11-21 18:18:46 +0100

    CPU: 0xFF/1 wrong arithm

--- vm/comp/instructionBlocks/cpu_special0xFF.c
+++ vm/comp/instructionBlocks/cpu_special0xFF.c
@@ -21,11 +21,11 @@ case 1: {
 		uint32_t src;
 		if(s_op.regmem_type == MEMORY){
 			src = cpu_read_word_from_mem(cpu_state, s_op.regmem_mem);
-			src++;
+			src--;
 			cpu_write_word_in_mem(cpu_state, src, s_op.regmem_mem);
 		} else {
 			src = cpu_read_word_from_reg(s_op.reg);
-			src++;
+			src--;
 			cpu_write_word_in_reg(s_op.regmem_reg, src);
 		}
 		return true;
