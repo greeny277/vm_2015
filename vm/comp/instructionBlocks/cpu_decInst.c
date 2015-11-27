397e65f6d7816b533991b3fea3313703737136a1
Christian Bay <christian.bay@studium.fau.de>
2015-11-27 14:54:09 +0100

    CPU: Shorten Inc and Dec cases

--- vm/comp/instructionBlocks/cpu_decInst.c
+++ vm/comp/instructionBlocks/cpu_decInst.c
@@ -1,143 +1,42 @@
-		case 0x48: {
-			/* Decrement word register eax by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->eax));
-			src--;
-			cpu_write_word_in_reg(&(cpu_state->eax), src);
+case 0x48 ... 0x4f: {
+	#ifdef DEBUG_PRINT_INST
+	switch(op_code & 0x7){
+		case 0:
+			fprintf(stderr, "DEC EAX\n");
+			break;
+		case 1:
+			fprintf(stderr, "DEC ECX\n");
+			break;
+		case 2:
+			fprintf(stderr, "DEC EDX\n");
+			break;
+		case 3:
+			fprintf(stderr, "DEC EBX\n");
+			break;
+		case 4:
+			fprintf(stderr, "DEC ESP\n");
+			break;
+		case 5:
+			fprintf(stderr, "DEC EBP\n");
+			break;
+		case 6:
+			fprintf(stderr, "DEC ESI\n");
+			break;
+		case 7:
+			fprintf(stderr, "DEC EDI\n");
+			break;
+	}
 
-			cpu_set_overflow_sub(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_sub(cpu_state, src+1, 1, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
+	#endif
+	/* Decrement word register eax-edi by 1 */
+	uint32_t src = cpu_read_word_from_reg(&((&cpu_state->eax)[op_code & 0x7]));
+	src--;
+	cpu_write_word_in_reg(&((&cpu_state->eax)[op_code & 0x7]), src);
 
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "DEC EAX \n");
-			#endif
+	cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
+	cpu_set_aux_flag_add(cpu_state, src+1, 1, src, !EIGHT_BIT);
+	cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
+	cpu_set_zero_flag(cpu_state, src);
 
-			return true;
-		}
-
-		case 0x49: {
-			/* Decrement word register ecx by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ecx));
-			src--;
-			cpu_write_word_in_reg(&(cpu_state->ecx), src);
-
-			cpu_set_overflow_sub(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_sub(cpu_state, src+1, 1, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "DEC ECX \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x4A: {
-			/* Decrement word register edx by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->edx));
-			src--;
-			cpu_write_word_in_reg(&(cpu_state->edx), src);
-
-			cpu_set_overflow_sub(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_sub(cpu_state, src+1, 1, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "DEC EDX \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x4B: {
-			/* Decrement word register ebx by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ebx));
-			src--;
-			cpu_write_word_in_reg(&(cpu_state->ebx), src);
-
-			cpu_set_overflow_sub(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_sub(cpu_state, src+1, 1, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "DEC EBX \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x4C: {
-			/* Decrement word register esp by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->esp));
-			src--;
-			cpu_write_word_in_reg(&(cpu_state->esp), src);
-
-			cpu_set_overflow_sub(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_sub(cpu_state, src+1, 1, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "DEC ESP \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x4D: {
-			/* Decrement word register ebp by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ebp));
-			src--;
-			cpu_write_word_in_reg(&(cpu_state->ebp), src);
-
-			cpu_set_overflow_sub(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_sub(cpu_state, src+1, 1, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "DEC EBX \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x4E: {
-			/* Decrement word register esi by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->esi));
-			src--;
-			cpu_write_word_in_reg(&(cpu_state->esi), src);
-
-			cpu_set_overflow_sub(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_sub(cpu_state, src+1, 1, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "DEC ESI \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x4F: {
-			/* Decrement word register edi by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->edi));
-			src--;
-			cpu_write_word_in_reg(&(cpu_state->edi), src);
-
-			cpu_set_overflow_sub(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_sub(cpu_state, src+1, 1, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "DEC EDI \n");
-			#endif
-
-			return true;
-		}
+	return true;
+}
--- vm/comp/instructionBlocks/cpu_incInst.c
+++ vm/comp/instructionBlocks/cpu_incInst.c
@@ -1,143 +1,42 @@
-		case 0x40: {
-			/* Increment word register eax by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->eax));
-			src++;
-			cpu_write_word_in_reg(&(cpu_state->eax), src);
+case 0x40 ... 0x47: {
+	#ifdef DEBUG_PRINT_INST
+	switch(op_code & 0x7){
+		case 0:
+			fprintf(stderr, "INC EAX\n");
+			break;
+		case 1:
+			fprintf(stderr, "INC ECX\n");
+			break;
+		case 2:
+			fprintf(stderr, "INC EDX\n");
+			break;
+		case 3:
+			fprintf(stderr, "INC EBX\n");
+			break;
+		case 4:
+			fprintf(stderr, "INC ESP\n");
+			break;
+		case 5:
+			fprintf(stderr, "INC EBP\n");
+			break;
+		case 6:
+			fprintf(stderr, "INC ESI\n");
+			break;
+		case 7:
+			fprintf(stderr, "INC EDI\n");
+			break;
+	}
 
-			cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_add(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
+	#endif
+	/* Increment word register eax-edi by 1 */
+	uint32_t src = cpu_read_word_from_reg(&((&cpu_state->eax)[op_code & 0x7]));
+	src++;
+	cpu_write_word_in_reg(&((&cpu_state->eax)[op_code & 0x7]), src);
 
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "INC EAX \n");
-			#endif
+	cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
+	cpu_set_aux_flag_add(cpu_state, src+1, 1, src, !EIGHT_BIT);
+	cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
+	cpu_set_zero_flag(cpu_state, src);
 
-			return true;
-		}
-
-		case 0x41: {
-			/* Increment word register ecx by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ecx));
-			src++;
-			cpu_write_word_in_reg(&(cpu_state->ecx), src);
-
-			cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_add(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "INC ECX \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x42: {
-			/* Increment word register edx by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->edx));
-			src++;
-			cpu_write_word_in_reg(&(cpu_state->edx), src);
-
-			cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_add(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "INC EDX \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x43: {
-			/* Increment word register ebx by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ebx));
-			src++;
-			cpu_write_word_in_reg(&(cpu_state->ebx), src);
-
-			cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_add(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "INC EBX \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x44: {
-			/* Increment word register esp by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->esp));
-			src++;
-			cpu_write_word_in_reg(&(cpu_state->esp), src);
-
-			cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_add(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "INC ESP \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x45: {
-			/* Increment word register ebp by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->ebp));
-			src++;
-			cpu_write_word_in_reg(&(cpu_state->ebp), src);
-
-			cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_add(cpu_state, src+1, 1, src, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "INC EBP \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x46: {
-			/* Increment word register esi by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->esi));
-			src++;
-			cpu_write_word_in_reg(&(cpu_state->esi), src);
-
-			cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "INC ESI \n");
-			#endif
-
-			return true;
-		}
-
-		case 0x47: {
-			/* Increment word register edi by 1 */
-			uint32_t src = cpu_read_word_from_reg(&(cpu_state->edi));
-			src++;
-			cpu_write_word_in_reg(&(cpu_state->edi), src);
-
-			cpu_set_overflow_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_aux_flag_add(cpu_state, src-1, 1, src, !EIGHT_BIT);
-			cpu_set_sign_flag(cpu_state, src, !EIGHT_BIT);
-			cpu_set_zero_flag(cpu_state, src);
-
-			#ifdef DEBUG_PRINT_INST
-			fprintf(stderr, "INC EDI \n");
-			#endif
-
-			return true;
-		}
+	return true;
+}
